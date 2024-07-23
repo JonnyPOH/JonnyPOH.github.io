4775870dabe7ed0597b330c057d05468d76d4c2e
JonnyPOH <jonny_o_hanlon@hotmail.co.uk>
2024-07-23 22:23:26 +0100

    q

--- test-main.js
+++ test-main.js
@@ -1,9 +1,12 @@
 import { JSDOM } from "jsdom";
-import "./src/main.js";
 
-// Create a fake DOM environment
+// Create a fake DOM environment before importing main.js
 const { window } = new JSDOM(`<!DOCTYPE html><canvas id="game"></canvas>`);
 global.document = window.document;
 global.window = window;
+global.navigator = window.navigator;
+
+// Now import main.js after setting up the DOM
+import "./src/main.js";
 
 console.log("Main.js has been executed");
