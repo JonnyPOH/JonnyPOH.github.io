3495af372957b4524fbf4a21a67af8f98d48186a
JonnyPOH <jonny_o_hanlon@hotmail.co.uk>
2024-07-23 22:21:29 +0100

    q

--- test-main.js
+++ test-main.js
@@ -1,3 +1,9 @@
-import "./src/main.js"; // Adjust the path if necessary
+import { JSDOM } from "jsdom";
+import "./src/main.js";
+
+// Create a fake DOM environment
+const { window } = new JSDOM(`<!DOCTYPE html><canvas id="game"></canvas>`);
+global.document = window.document;
+global.window = window;
 
 console.log("Main.js has been executed");
