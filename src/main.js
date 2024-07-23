ad2359cb90441427013c31bbed4dd53c079b5954
JonnyPOH <jonny_o_hanlon@hotmail.co.uk>
2024-07-23 23:36:21 +0100

    q

--- src/main.js
+++ src/main.js
@@ -9,9 +9,21 @@ export const k = kaboom({
   debug: true,
 });
 
+console.log("Kaboom initialized");
+
+// Simple check to confirm the environment
+if (!k) {
+  console.error("Kaboom initialization failed");
+  process.exit(1);
+} else {
+  console.log("Kaboom initialization successful");
+}
+
 console.log("Setting background color");
 
 k.setBackground(k.Color.fromHex("#311047"));
+
+console.log("Background color set");
 
 k.scene("main", () => {
   console.log("Main scene started");
@@ -19,8 +31,11 @@ k.scene("main", () => {
     k.text("Hello Kaboom", { size: 32 }),
     k.pos(100, 100),
   ]);
+  console.log("Text added to scene");
 });
 
 console.log("Starting main scene");
 
 k.go("main");
+
+console.log("Main scene started");
