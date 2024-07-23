220f39f7cd6cd16a4a6fc4680c236343fc23621d
JonnyPOH <jonny_o_hanlon@hotmail.co.uk>
2024-07-23 21:56:50 +0100

    q

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
-import { k } from "./src/kaboomCtx";
+import kaboom from "kaboom";
+// import { scaleFactor } from "./constants";
+
+export const k = kaboom({
+  global: false,
+  touchToMouse: true,
+  canvas: document.getElementById("game"),
+  debug: false, // set to false once ready for production
+});
+
+
+// import { k } from "./kaboomCtx";
 
 // Simple test to set background color
 k.setBackground(k.Color.fromHex("#311047"));
 
-k.scene("main", () => {
-  // Your scene setup code here
-});
+// k.scene("main", () => {
+//   // Your scene setup code here
+// });
 
 k.go("main");
